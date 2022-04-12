efc235ed23b5cf477b383ac1fb89a902b92d88d9
Ravindra <63548064+ravi814@users.noreply.github.com>
2022-04-12 10:20:07 +0530

    Spring MVC

---
+++ SpringMVCQ/src/main/java/com/Q1/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.Q1;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/index")
+	public ModelAndView indexView() {
+		ModelAndView model = new ModelAndView("index");
+		return model;
+	}
+	
+	@RequestMapping("/HelloWorld")
+	public ModelAndView helloWorld() {
+		ModelAndView model = new ModelAndView("helloWorld");
+		return model;
+	}
+
+}
